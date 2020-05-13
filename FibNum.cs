f94c888d3b9ba494b884f009435562b64dc5fcdb
vldmrklnk <uladzimerkalenik@gmail.com>
2020-05-13 21:54:24 +0300

    Добавьте файлы проекта.

---
+++ FibNum.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hm15
+{
+	public class FibNum
+	{
+		public FibNum(uint n)
+		{
+			uint[] mass = new uint[2] { 0, 1 };
+			uint i = 0;
+			uint a = 0;
+			while (i <= n)
+			{
+				Console.WriteLine(mass[0]);
+				a = mass[0] + mass[1];
+				mass[0] = mass[1];
+				mass[1] = a;
+				i++;
+				
+			}
+		
+			
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hm15
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			FibNum fibNum = new FibNum(10);
+			Console.ReadLine();
+		}
+	}
+}
